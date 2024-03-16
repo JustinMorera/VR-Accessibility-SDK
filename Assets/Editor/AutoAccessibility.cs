81ab390bddcd317b82c29742b59dd37cdaaf9a0f
JustinMorera <ju691930@ucf.edu>
2024-03-16 12:39:04 -0400

    Revert change to AutoAccessibility

--- Assets/Editor/AutoAccessibility.cs
+++ Assets/Editor/AutoAccessibility.cs
@@ -7,7 +7,7 @@ using AccessibilityTags;
 
 // Editor script that Adds AltText to ALL GameObjects
 [CustomEditor(typeof(GameObject))]
-public class AutoAccessibility : ScriptableObject
+public class AutoAccessibility : Editor
 {    
     // Right-click option for GameObjects
     [MenuItem("Tools/Add Accessible Field(s) to entire scene")]
